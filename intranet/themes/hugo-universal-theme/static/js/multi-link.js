d5979fc5ba030d765766086d03a445a9bc06323d
Jaki Frisina <jaki.frisina@leblibrary.com>
2019-06-21 16:05:00 -0400

    re-added hugo universal theme

---
+++ intranet/themes/hugo-universal-theme/static/js/multi-link.js
@@ -0,0 +1,7 @@
+function multiLink() {
+  window.open("https://adpeet2.adp.com/102tc7p/logon/logonWFC.html");
+  window.open("https://leblibrary.slack.com/");
+  window.open("http://mail.google.com/");
+
+
+};
