c15554ff5f17e15c7c86552efc47189708ce37a4
Jaki Frisina <jaki.frisina@leblibrary.com>
2019-11-27 16:53:11 -0500

    changed active status on sidenav

--- intranet/themes/hugo-universal-theme/static/js/active.js
+++ intranet/themes/hugo-universal-theme/static/js/active.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-    $("nav-item li").on("click", function() {
-        $("nav-item li").removeClass("active");
-        $(this).addClass("active");
+    $(".nav li").click(function(){
+         $(this).parent().children("li").removeClass("active");
+         $(this).toggleClass("active");
     });
 });
