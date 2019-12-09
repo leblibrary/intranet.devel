40e6e85ab6bbbc3161db53c849fa38fb7b60363f
Jaki Frisina <jaki.frisina@leblibrary.com>
2019-12-09 14:27:59 -0500

    fixed bug in dropdown menu, added readers advisory feature

--- intranet/themes/hugo-universal-theme/static/js/active.js
+++ intranet/themes/hugo-universal-theme/static/js/active.js
@@ -1,6 +1,8 @@
-$(document).ready(function() {
+/* $(document).ready(function() {
     $(".nav li").click(function(){
          $(this).parent().children("li").removeClass("active");
          $(this).toggleClass("active");
     });
 });
+
+*/
